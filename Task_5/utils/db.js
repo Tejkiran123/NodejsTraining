6f3e0036a974116fca845e35019d2f6bde1c4c9a
Tejkiran123 <119047203+Tejkiran123@users.noreply.github.com>
2022-12-12 13:31:52 +0530

    Add files via upload

---
+++ Task_5/utils/db.js
@@ -0,0 +1,11 @@
+const db = {
+    items: [{
+        "name": "Super man",
+        "price": "30.5",
+        "Published": "KiranPublisher",
+        "md": "31/12/2012"
+    }],
+    users: []
+}
+
+module.exports = db;
